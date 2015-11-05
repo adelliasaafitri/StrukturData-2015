1a69ef97f87e16dd47bc7a78425492a4a173007e
adelliasaafitri <adeliasafitri96@gmail.com>
2015-11-05 20:39:38 +0700

    no message

---
+++ 02-Pool-Kendaraan/Becak.java
@@ -0,0 +1,17 @@
+public class Becak implements Kendaraan {
+    private String plat;
+    
+    public Becak(String plat){
+        this.plat = plat;
+    }
+    
+    public String plat(){
+        return plat;
+    }
+    
+    public String tipe(){
+        String tipe = new String("Becak");
+        return tipe;
+
+    }
+}
