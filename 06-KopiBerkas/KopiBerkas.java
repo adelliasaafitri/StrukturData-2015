21ac5222f7d2c4df1b83d3332380b632b95fe531
adelliasaafitri <adeliasafitri96@gmail.com>
2015-11-06 16:26:46 +0700

    no message

--- 06-KopiBerkas/KopiBerkas.java
+++ 06-KopiBerkas/KopiBerkas.java
@@ -3,6 +3,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class KopiBerkas {
+    public static void main (String[] args){
+        try {
+            KopiBerkas x = new KopiBerkas();
+            x.kopi("README.txt","kopi.txt");
+        }
+        catch (IOException kesalahan){
+            System.out.printf("Terjadi kesalahan : %s",kesalahan);
+        }
+    }
+    
    public void kopi(String sumber, String sasaran) throws IOException {
         FileInputStream masukan = null;
         FileOutputStream keluaran = null;
